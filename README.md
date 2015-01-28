8ee2686db40664b38d83acbe0c1221751a331de6
Paul Ollivier <contact@paulollivier.fr>
2015-01-28 17:20:13 +0100

    Initial commit; first prototype.

---
+++ README.md
@@ -0,0 +1,51 @@
+# xpose-generator
+
+This simple script is something I did rather quickly to generate the website we
+needed to create in the contexte of the [presentations done in third year of CS
+engineering at ESIPE][1].
+
+It is very simple: it takes the inforamtion describe in `a`, a text file
+containing meta-information, and every markdown file it finds in `content`, and
+generates the proper html tree in `build`.
+
+## Installation
+
+    git clone <url_of_the_git_repository_you_are_viewing_this_file_on>
+    cd xpose-generator
+    pip install .
+
+
+Once this is done, you can call `xposegen`, in the directory of your choice.
+
+## How to write content:
+
+Simply write a .md file in `content/`, write a line `Title: My Title Here` on
+top of the file, and then write your content. Note: For presentation purpose,
+don't write `<h1>` tags, aka `#`, in markdown. 
+
+here be samples:
+
+    Title: This is my test page
+    ## Hello, world
+
+    Hello, world. I am writing some stuff in markdown.
+    [This is a link to another page, with header][hello.html#title]
+
+If you have images, etc... just put it in `content/`
+
+
+# Contributing
+
+Please, feel free to clone the repository, make your stuff, and eventually make
+a pull request to merge what you did? That would be nice.
+
+Also, even if you don't want to merge you code, an email will be appreciated.
+
+## TODO
+
+* Add support for custom css
+* do not hard-code everything; add support for cli args, or conffile
+* Decrease every header if a `# title` is detected.
+
+
+[1]: http://www-igm.univ-mlv.fr/~dr/xall.php
---
+++ xpose_gen/main.py
@@ -0,0 +1,97 @@
+# -*- coding: utf8 -*-
+import os
+import re
+import codecs
+from markdown import Markdown
+from jinja2 import Template
+import shutil
+
+site_template = Template(u'''
+<!doctype html>
+<html>
+<head>
+<title>{{site_title}}: {{page_title}}</title>
+<link rel="stylesheet" type="text/css" title="Xposés" href="http://www-igm.'''
+                         '''univ-mlv.fr/~dr/XPOSE/style.css"/>
+<meta charset='utf-8' />
+</head>
+<body>
+<div id="content">
+<h1>{{site_title}}</h1>
+{{content}}
+</div>
+<div id="navigation">
+{% for page in navigation %}
+<h2>{{page.title}}</h2>
+<ul>
+{% for header in page.headers %}
+<li><a href={{page.path}}#{{header.id}}>{{header.title}}</a></li>
+{% endfor %}
+</ul>
+{% endfor %}
+</div>
+''')
+
+
+class A(object):
+    def __init__(self, fname='a'):
+        with codecs.open(fname, encoding='utf-8') as f:
+            self.a = f.read()
+        t = self.a.split('\n')
+        self.author = t[0]
+        self.title = t[1]
+        self.classifiers = t[2:]
+
+
+content_file_re = re.compile(r'[A-Za-z0-9\-_]+\.md')
+exts = ['markdown.extensions.' + ext
+        for ext in ['extra', 'admonition', 'codehilite', 'headerid',
+                    'meta', 'sane_lists']]
+
+
+def copy_ressources(frompath='content', to='build', types=r'png|jpg|gif'):
+    for fname in os.listdir(frompath):
+        if not re.match(".*\.(" + types + ")", fname):
+            continue
+        path = os.path.sep.join([frompath, fname])
+        shutil.copy2(path, os.path.sep.join([to, fname]))
+
+
+def main():
+    files = []
+    md = Markdown(extensions=exts, output_format='html5')
+    for path in os.listdir(os.path.sep.join([os.getcwd(), 'content'])):
+        if not content_file_re.match(path):
+            continue
+        p = os.path.sep.join(os.path.join([os.getcwd(), "content", path]))
+        with codecs.open(p, encoding='utf-8') as f:
+            content = f.read()
+        html = md.convert(content)
+        titles = []
+        h2_re = re.compile(
+            r'<h2 id="([a-z0-9\-]+)">([_a-zA-Z0-9\-\ \!\,]+)</h2>')
+        for line in html.splitlines():
+            match = h2_re.match(line)
+            if match:
+                titles.append({'id': match.group(1),
+                               'title': match.group(2)})
+        files.append({'path': path, 'title': md.Meta['title'][0],
+                      'headers': titles, 'html': html})
+    print(files)
+    a = A()
+    for d in files:
+        real_html = site_template.render(site_title=a.title,
+                                         page_title=d['title'],
+                                         content=d['html'],
+                                         navigation=files)
+        path = d['path'].replace('.md', '.html')
+        if not os.path.exists('build'):
+            os.mkdir('build')
+        with codecs.open(os.path.sep.join(['build', path]),
+                         'w+',
+                         encoding='utf-8') as f:
+            f.write(real_html)
+    copy_ressources()
+
+if __name__ == '__main__':
+    main()
